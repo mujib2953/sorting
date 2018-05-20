69654dd2727d39a1d3cb65c79248b0a10088e3ae
si-mujib-ansari <mujibur@sportzinteractive.net>
2018-05-20 15:13:17 +0530

    basic Bubble sort implemented.

--- src/common/js/index.js
+++ src/common/js/index.js
@@ -2,10 +2,46 @@
  * @Author: Mujib Ansari 
  * @Date: 2018-05-17 15:53:01 
  * @Last Modified by: Mujib Ansari
- * @Last Modified time: 2018-05-17 15:53:29
+ * @Last Modified time: 2018-05-17 17:59:36
  */
 
 
 const com = {};
+
+com.getRandom = ( p_min, p_max ) => {
+
+    let nMin = Math.floor( p_min ? p_min : 0 ),
+        nMax = Math.ceil( p_max ? p_max : 450 );
+
+    return Math.floor( Math.random() * ( nMax - nMin ) ) + nMin;
+
+}
+
+com.generate50Values = () => {
+
+    let aTest = [];
+
+    for( let i = 0; i < 50; i++ ) {
+        
+        aTest.push( com.getRandom() );
+        
+    }
+
+    return aTest;
+}
+
+com.getRandomColor = () => {
+
+    const aColor = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F' ];
+
+    let nLen = aColor.length,
+        r = aColor[ com.getRandom( 0, nLen ) ],
+        g = aColor[ com.getRandom( 0, nLen ) ],
+        b = aColor[ com.getRandom( 0, nLen ) ];
+
+    return '#' + r + g + b;
+
+}
+
 
 export { com as default }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,58 @@
  * @Author: Mujib Ansari 
  * @Date: 2018-05-17 15:53:40 
  * @Last Modified by: Mujib Ansari
- * @Last Modified time: 2018-05-17 16:01:45
+ * @Last Modified time: 2018-05-17 18:19:25
  */
 import React from 'react';
+
+import Canvas from './Canvas';
+
+import com from '../common/js'
 
 class Home extends React.Component {
     render() {
         return(
             <div className="homeConatiner jumbotron">
-                This is Home
+                {
+                    this.renderCanvas()
+                }
             </div>
         )
+    }
+
+    renderCanvas = () => {
+        
+        let markup = null,
+            aData = com.generate50Values(),
+            oTemp = [];
+
+        for( let i in aData ) {
+            oTemp.push(
+                {
+                    value: aData[ i ],
+                    ogColor: com.getRandomColor(),
+                    tempColor: ''
+                }
+            )
+        }
+
+        console.log( oTemp );
+
+
+
+        markup = (
+            <Canvas 
+                id = "test"
+                aLineData = { oTemp }
+            />
+        );
+
+
+        return markup;
+    }
+
+    doRecursiveBubbleSrt = () => {
+        
     }
 }
 
